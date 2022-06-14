8dd250f094ea7885eea3845b9a0db4360518ae51
Darryl Le Roux <devdarryl@outlook.com>
2022-06-14 21:18:51 +0200

    Outputting dynamic data

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  const expenseDate = new Date(2022, 5, 14);
+  const expenseItem = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description ">
+        <h2>{expenseItem}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
