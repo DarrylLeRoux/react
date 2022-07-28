b63421dfc069ea7cb12d29d8b4d2324b76e24173
Darryl Le Roux <devdarryl@outlook.com>
2022-07-28 23:05:08 +0200

    Lifting state up

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -3,10 +3,21 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpensehandler = (enteredExpenseData) => {
+    //Define the function to pass the prop
+    const expenseData = {
+      // destructure the enteredExpenseData from the passed argument
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+      // Set and id to a random number
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpensehandler} />
     </div>
   );
 };
