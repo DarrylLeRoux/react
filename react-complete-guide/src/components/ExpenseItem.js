844c361382a2c0ceffd0a601a1c982c982f9e46b
Darryl Le Roux <devdarryl@outlook.com>
2022-11-06 22:52:32 +0200

    stored data in variables for use elsehwere? ?

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurane";
+  const expenseAmount = 294.68;
+
   return (
-    <div>
-      <div>March 28 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
