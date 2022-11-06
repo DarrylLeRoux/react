174361e1f859d8cbc1cae12b49f0b2a8cbbdc4b0
Darryl Le Roux <devdarryl@outlook.com>
2022-11-06 22:29:07 +0200

    Added basic component filled

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,6 +2,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
+      <h2>Let's get Started!</h2>
       <ExpenseItem />
     </div>
   );
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,7 +1,11 @@
 function ExpenseItem() {
   return (
     <div>
-      <h2></h2>
+      <div>March 28 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
     </div>
   );
 }
