0c0478a5414d3d0ad0bebdc01bf7b45ae2b4599e
Darryl Le Roux <devdarryl@outlook.com>
2022-11-08 17:44:57 +0200

    Added classes to the custom component for styling

--- react-complete-guide/src/components/Card.js
+++ react-complete-guide/src/components/Card.js
@@ -1,8 +1,9 @@
 import "./Card.css";
 
 function Card(props) {
+  const classes = "card " + props.className;
   return (
-    <div className="card + props.children">
+    <div className={classes}>
       {/* props.children adds whatever is being passed from your custom component  */}
       {props.children}
       {/* This is like the App root in the index.js where the code is injected. Hence using it as a <Card></Card> on ExpenseItem*/}
